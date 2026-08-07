e4533c7d3d92ff0abb59991deef368fd1f7c6cbf
ama <sveps29@gmail.com>
2026-08-07 23:11:19 +0300

    undoed everything

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from discord import app_commands
 from discord.ext import tasks
 
+# REPLACE WITH A PER-SERVER SOLUTION
 try:
     with open("token.txt") as f:
         TOKEN = f.readline().strip()
@@ -13,7 +14,6 @@ try:
 except:
     print(f"ERROR: NO token.txt FILE FOUND. PLEASE CREATE A TXT FILE CALLED 'token.txt' AND ADD YOUR BOT TOKEN INSIDE OF IT")
 
-# REPLACE WITH A PER-SERVER SOLUTION
 try:
     with open("role.txt") as f:
         pingrole = f.readline().strip()
@@ -33,6 +33,15 @@ class MyClient(discord.Client):
     async def setup_hook(self):
         await self.tree.sync()
 
+    async def on_message(self, message): 
+        if message.author == 302050872383242240: 
+            if 'Bump done!' in message.embeds[0].description:
+                await asyncio.sleep(7199)
+                await message.channel.send(f'{pingrole}')
+        if message.author == 1155392571569356880: 
+            if message.content == "testing":
+                await asyncio.sleep(3)
+                await message.channel.send(f'{pingrole}')
 
 client = MyClient(intents=intents, allowed_mentions=all)
 
@@ -45,10 +54,11 @@ async def on_ready():
 @client.tree.command(name="configure", description="configure the bot")
 async def configure(interaction: discord.Interaction, role: discord.Role):
     pingrole = role.mention
-    await interaction.reply.send_message(content=f"{pingrole} from now on you will get pinged whenever bumping is possible. Thank you for bumping our server.")
+    await interaction.response.send_message(content=f"{pingrole} from now on you will get pinged whenever bumping is possible. Thank you for bumping our server.")
     with open("role.txt", "w", encoding="utf-8") as f:
             f.write(pingrole)
             f.close()
+
 
 
 client.run(TOKEN)
