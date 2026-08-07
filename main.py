bc4cf670de389502ba158cea6860b8b687131218
ama <sveps29@gmail.com>
2026-08-07 23:08:02 +0300

    some fixing

--- main.py
+++ main.py
@@ -33,15 +33,6 @@ class MyClient(discord.Client):
     async def setup_hook(self):
         await self.tree.sync()
 
-    async def on_message(self, message): 
-        if message.author == 302050872383242240: 
-            if 'Bump done!' in message.embeds[0].description:
-                await asyncio.sleep(7199)
-                await message.channel.send(f'{pingrole}')
-        if message.author == 1155392571569356880: 
-            if 'test' in message.description:
-                await asyncio.sleep(3)
-                await message.channel.send(f'{pingrole}')
 
 client = MyClient(intents=intents, allowed_mentions=all)
 
@@ -54,7 +45,7 @@ async def on_ready():
 @client.tree.command(name="configure", description="configure the bot")
 async def configure(interaction: discord.Interaction, role: discord.Role):
     pingrole = role.mention
-    await channel.send_message(content=f"{pingrole} from now on you will get pinged whenever bumping is possible. Thank you for bumping our server.")
+    await interaction.reply.send_message(content=f"{pingrole} from now on you will get pinged whenever bumping is possible. Thank you for bumping our server.")
     with open("role.txt", "w", encoding="utf-8") as f:
             f.write(pingrole)
             f.close()
